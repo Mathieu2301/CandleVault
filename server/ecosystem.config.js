8dd36865eaa69bc1ac0ca22c92a6ad6d2bf2769d
Mathieu Colmon <mathieu@usp-3.fr>
2021-04-25 23:29:25 +0200

    Init pm2 deploy

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  apps: [
-    {
-      name: 'candlevault-worker-default',
-      script: './main.js',
-      max_memory_restart: '100M',
+  apps: [{
+    name: 'CandleVault',
+    script: 'main.js',
+    watch: '.',
+  }],
+
+  deploy: {
+    production: {
+      user: 'main',
+      host: '192.168.0.1',
+      ref: 'origin/main',
+      repo: 'git@github.com:Mathieu2301/CandleVault.git',
+      path: '/home/main/CandleVault',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'pre-deploy-local': '',
+      'post-deploy': 'cd server && npm i && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
     },
-  ],
+  },
 };
